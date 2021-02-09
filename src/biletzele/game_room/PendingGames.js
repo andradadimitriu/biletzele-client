c10acf996f56d6d7730b9fbac0c26faf01ee7fd6
andradadimitriu <andrada.dimitriu@gmail.com>
2021-02-09 20:29:19 +0200

    search box for pending games

--- src/biletzele/game_room/PendingGames.js
+++ src/biletzele/game_room/PendingGames.js
@@ -4,6 +4,7 @@ import {GAME_STATUS} from "../utils/constants";
 import {getGamesByStatus} from "../service/biletzele-service";
 import Loading from "../../utils_components/Loading";
 import {Auth} from "aws-amplify";
+import Form from 'react-bootstrap/Form';
 
 export class PendingGames extends React.Component {
 
@@ -16,13 +17,25 @@ export class PendingGames extends React.Component {
       const pendingGames = await getGamesByStatus(GAME_STATUS.PENDING);
       //TODO might be able to get it in props
       const currentUser = await Auth.currentCredentials();
-      this.setState({currentUser, pendingGames, isLoading: false});
+      this.setState({currentUser, pendingGames, isLoading: false, filteredGames: pendingGames});
   }
+  async componentDidUpdate(prevProps, prevState){
+      if (prevState.searchText !== this.state.searchText) {
+          if(this.state.searchText.length > 0){
+              this.setState({filteredGames: this.state.pendingGames.filter(game => game.gameName.toLowerCase().includes(this.state.searchText.toLowerCase()))});
+          }
+      }
+
+  }
+
+  async
 
   render() {
       return (
       <div>
-        {this.state.isLoading ? <Loading/> : this.state.pendingGames.map((game, id) => <GameTile key={id} game={game} user={this.state.currentUser}/>)}
+          <Form.Control style={{margin: 20,  width: '24rem'}} type="text" placeholder="Search" value={this.state.searchText} className="mr-sm-2" onChange={(event)=>this.setState({searchText: event.target.value})}
+          />
+        {this.state.isLoading ? <Loading/> : this.state.filteredGames.map((game, id) => <GameTile key={id} game={game} user={this.state.currentUser}/>)}
       </div>
       );
   }
