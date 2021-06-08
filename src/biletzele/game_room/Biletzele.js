e0ed186cfc621a33260f6f6475d2aa1c97d64668
andradadimitriu <andrada.dimitriu@gmail.com>
2021-06-08 17:51:13 +0300

    phone friendly display

--- src/biletzele/game_room/Biletzele.js
+++ src/biletzele/game_room/Biletzele.js
@@ -26,7 +26,7 @@ export class Biletzele extends React.Component {
 
   render() {
       return (
-      <div className="horizontalflex margin" style={{alignItems: "center"}}>
+      <div className="horizontalflex margin">
           <ToggleButtonGroup type="radio" value={this.state.value} name="radioAll" onChange={this.handleChange}>
                         <ToggleButton variant="light" name="radio1" value={"join"}>Join Game</ToggleButton>
                         <ToggleButton variant="light" name="radio2" value={"mine"}>My Games</ToggleButton>
